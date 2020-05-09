623d7d92c5991551355833cdc004fbed2f20b7fd
Aplet123 <aplet@aplet.me>
2020-05-09 14:39:19 -0400

    Move tests to separate files and rename.

---
+++ tests/header.rs
@@ -0,0 +1,41 @@
+extern crate rusty_ws;
+use rusty_ws::http;
+
+mod common;
+
+#[test]
+fn success_standard() {
+    // Parsing should work as expected.
+    let req = http::Header::parse("Referer: \t\t http://www.example.com/").unwrap();
+    assert_eq!(req.name, "Referer");
+    assert_eq!(req.value, "http://www.example.com/");
+}
+
+#[test]
+fn success_empty_value() {
+    // Empty values are allowed.
+    let req = http::Header::parse("Referer: \t\t ").unwrap();
+    assert_eq!(req.name, "Referer");
+    assert_eq!(req.value, "");
+}
+
+#[test]
+#[should_panic]
+fn fail_no_value() {
+    // Having no value should fail.
+    http::Header::parse("Referer").unwrap();
+}
+
+#[test]
+#[should_panic]
+fn fail_empty_name() {
+    // Having no name should fail.
+    http::Header::parse(": test").unwrap();
+}
+
+#[test]
+#[should_panic]
+fn fail_malformed_name() {
+    // Having separators in the name should fail.
+    http::Header::parse("Test Header: test").unwrap();
+}
---
+++ tests/request_line.rs
@@ -0,0 +1,50 @@
+extern crate rusty_ws;
+use rusty_ws::http;
+
+mod common;
+
+#[test]
+fn success_standard() {
+    // Parsing should work as expected.
+    let req = http::RequestLine::parse("POST /abc/def HTTP/1.1").unwrap();
+    assert_eq!(req.method, http::RequestMethod::Post);
+    assert_eq!(req.path, "/abc/def");
+    assert_eq!(req.version, "1.1");
+}
+
+#[test]
+fn success_other_method() {
+    // Non-documented methods should also work.
+    let req = http::RequestLine::parse("PATCH /abc/def HTTP/1.1").unwrap();
+    assert_eq!(req.method, http::RequestMethod::Other("PATCH"));
+    assert_eq!(req.path, "/abc/def");
+    assert_eq!(req.version, "1.1");
+}
+
+#[test]
+#[should_panic]
+fn fail_extra_1() {
+    // Extra clauses should error.
+    http::RequestLine::parse("POST /abc/def HTTP/1.1 x").unwrap();
+}
+
+#[test]
+#[should_panic]
+fn fail_extra_2() {
+    // Extra clauses should error.
+    http::RequestLine::parse("POST /a /b HTTP/1.1").unwrap();
+}
+
+#[test]
+#[should_panic]
+fn fail_malformed_version() {
+    // Malformed versions should error.
+    http::RequestLine::parse("POST /abc/def HTDP/1.1").unwrap();
+}
+
+#[test]
+#[should_panic]
+fn fail_missing_clause() {
+    // Missing clauses should error.
+    http::RequestLine::parse("POST /abc/def").unwrap();
+}

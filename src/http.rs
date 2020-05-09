6e6ee3fa2630e7e32bbf904f79f591747e53124a
Aplet123 <aplet@aplet.me>
2020-05-09 09:02:43 -0400

    unit testing

--- src/http.rs
+++ src/http.rs
@@ -1,32 +1,34 @@
 use crate::constants::*;
 
 pub struct RequestLine {
-    method: String,
-    path: String,
-    version: String,
+    pub method: String,
+    pub path: String,
+    pub version: String,
 }
 
-pub fn parse_request_line(request_line: String) -> Option<RequestLine> {
-    let mut toks = request_line.split(SP);
-    let method = match toks.next() {
-        Some(v) => v,
-        None => return None
-    };
-    let path = match toks.next() {
-        Some(v) => v,
-        None => return None
-    };
-    let version = match toks.next() {
-        Some(v) => v,
-        None => return None
-    };
-    let (first, ver) = version.split_at(5);
-    if first != "HTTP/" {
-        return None;
+impl RequestLine {
+    pub fn parse(request_line: String) -> Option<Self> {
+        let mut toks = request_line.split(SP);
+        let method = match toks.next() {
+            Some(v) => v,
+            None => return None
+        };
+        let path = match toks.next() {
+            Some(v) => v,
+            None => return None
+        };
+        let version = match toks.next() {
+            Some(v) => v,
+            None => return None
+        };
+        let (first, ver) = version.split_at(5);
+        if first != "HTTP/" || toks.next().is_some() {
+            return None;
+        }
+        Some(RequestLine {
+            method: method.to_string(),
+            path: path.to_string(),
+            version: ver.to_string(),
+        })
     }
-    Some(RequestLine {
-        method: method.to_string(),
-        path: path.to_string(),
-        version: ver.to_string(),
-    })
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,34 @@
-mod http;
 mod constants;
+mod http;
 
 #[cfg(test)]
 mod tests {
+    use crate::http;
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn request_line_success() {
+        let req = http::RequestLine::parse("POST /abc/def HTTP/1.1".to_string()).unwrap();
+        assert_eq!(req.method, "POST");
+        assert_eq!(req.path, "/abc/def");
+        assert_eq!(req.version, "1.1");
+    }
+
+    #[test]
+    fn request_line_fail_1() {
+        assert!(http::RequestLine::parse("POST /abc/def HTTP/1.1 x".to_string()).is_none());
+    }
+
+    #[test]
+    fn request_line_fail_2() {
+        assert!(http::RequestLine::parse("POST /abc/def HTDP/1.1".to_string()).is_none());
+    }
+
+    #[test]
+    fn request_line_fail_3() {
+        assert!(http::RequestLine::parse("POST /abc/def".to_string()).is_none());
     }
 }

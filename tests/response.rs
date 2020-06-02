f613363bbd49d8ed7814f341bcf82a12a2b3e4c2
weegee <somilsharma627@gmail.com>
2020-06-03 02:46:10 +0530

    fix unit tests

--- tests/response.rs
+++ tests/response.rs
@@ -1,21 +1,19 @@
 extern crate octane;
-use octane::responder::Response;
 use octane::constants::StatusCode;
+use octane::responder::Response;
 
 #[test]
 fn success_standard() {
     // default response should provide OK 200 Code
-    let req = Response::new(b"").get_string();
+    let req = Response::new(b"").get_data();
     assert_eq!(String::from_utf8(req).unwrap(), "HTTP/1.1 200 OK\r\n\r\n");
 }
 
 #[test]
 fn response_with_status_code_different() {
     // Reponse with different status codes should work
-    let req = Response::new(b"")
-        .with_status(StatusCode::Created)
-        .get_string();
-    
+    let req = Response::new(b"").status(StatusCode::Created).get_data();
+
     assert_eq!(
         String::from_utf8(req).unwrap(),
         "HTTP/1.1 201 CREATED\r\n\r\n"
@@ -27,8 +25,8 @@ fn response_with_different_http_version() {
     // Reponse with different status codes should work
     let req = Response::new(b"")
         .with_http_version("x.y")
-        .with_status(StatusCode::Created)
-        .get_string();
+        .status(StatusCode::Created)
+        .get_data();
     assert_eq!(
         String::from_utf8(req).unwrap(),
         "HTTP/x.y 201 CREATED\r\n\r\n"

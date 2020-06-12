c61b13ce8080bfc06373f8608aed3642f3fd8d71
Aplet123 <aplet@aplet.me>
2020-06-12 12:50:35 -0400

    Matching and subtraction for PathBuf.

--- src/path.rs
+++ src/path.rs
@@ -1,5 +1,6 @@
 use std::convert::TryFrom;
 use std::ops::Deref;
+use std::collections::HashMap;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct PathBuf {
@@ -29,6 +30,42 @@ impl PathBuf {
             chunks.push(chunk.to_owned());
         }
         Ok(PathBuf { chunks })
+    }
+
+    #[cfg(feature = "url_variables")]
+    pub fn check_matches(&self, other: &PathBuf) -> Option<HashMap<String, String>> {
+        if self.len() != other.len() {
+            return None;
+        }
+        let mut vars = HashMap::new();
+        for (a, b) in self.iter().zip(other.iter()) {
+            if a.as_bytes()[0] == b':' {
+                vars.insert(a[1..].to_owned(), b.clone());
+            } else if a != b {
+                return None;
+            }
+        }
+        Some(vars)
+    }
+
+    #[cfg(not(feature = "url_variables"))]
+    pub fn check_matches(&self, other: &PathBuf) -> Option<()> {
+        if self.iter().ne(other.iter()) {
+            return None;
+        }
+        Some(())
+    }
+
+    pub fn subtract(&self, other: &PathBuf) -> Option<PathBuf> {
+        if self.len() < other.len() {
+            return None;
+        }
+        if self.iter().take(other.len()).ne(other.iter()) {
+            return None;
+        }
+        Some(PathBuf {
+            chunks: self.chunks[other.len()..].to_vec()
+        })
     }
 }
 
--- tests/path.rs
+++ tests/path.rs
@@ -9,6 +9,30 @@ fn success_standard() {
 }
 
 #[test]
+fn success_matching() {
+    // Parsing should work as expected.
+    let path1 = PathBuf::parse("/asdf/:var/foo/").unwrap();
+    let path2 = PathBuf::parse("asdf/test/foo/").unwrap();
+    let path3 = PathBuf::parse("/asdf/test/foo").unwrap();
+    let path4 = PathBuf::parse("/asdf/test/bad").unwrap();
+    assert_eq!(path1.check_matches(&path2).unwrap()["var"], "test".to_string());
+    assert_eq!(path1.check_matches(&path3).unwrap()["var"], "test".to_string());
+    assert!(path1.check_matches(&path4).is_none());
+}
+
+#[test]
+fn success_subtraction() {
+    // Parsing should work as expected.
+    let path1 = PathBuf::parse("/a/b/c/d/").unwrap();
+    let path2 = PathBuf::parse("/a/b/").unwrap();
+    let path3 = PathBuf::parse("/c/d").unwrap();
+    let path4 = PathBuf::parse("a/:x/c").unwrap();
+    assert_eq!(path1.subtract(&path2).unwrap().chunks, vec!["c", "d"]);
+    assert!(path1.subtract(&path3).is_none());
+    assert!(path1.subtract(&path4).is_none());
+}
+
+#[test]
 #[should_panic]
 fn fail_traversal() {
     // Too many ..s should error.
